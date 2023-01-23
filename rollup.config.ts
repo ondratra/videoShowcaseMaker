ae4eb5e007b58deb85c35d2f49b37558b3b6e521
ondratra <ondratra@gmail.com>
2023-01-23 22:31:35 +0100

    build system improvements

--- rollup.config.ts
+++ rollup.config.ts
@@ -1,18 +1,40 @@
 import glob from 'glob'
 import rollupTypescript from '@rollup/plugin-typescript'
+import dts from "rollup-plugin-dts";
+import {Plugin as RollupPlugin} from 'rollup';
 
-const baseConfig = (input: string, output: string) => ({
+// basic rollup config setup
+const baseConfig = (input: string, output: string, plugins: RollupPlugin[]) => ({
   input: input,
     output: {
       file: output,
       format: 'es',
     },
     preserveModules: false,
-    plugins: [rollupTypescript()],
+    plugins,
 })
 
+// common plugins setup
+const commonPlugins = (tsconfigPath: string) => [
+  rollupTypescript({tsconfig: tsconfigPath})
+]
+
 export default [
-  baseConfig('src/index.ts', 'dist/index.js'),
+  // library
+  baseConfig('src/index.ts', 'dist/index.js', commonPlugins('tsconfig.core.json')),
+
+  // library typings
+  {
+    input: 'src/index.ts',
+      output: {
+        file: 'dist/index.d.ts',
+        format: 'es',
+      },
+      preserveModules: false,
+      plugins: [dts()],
+  },
+
+  // examples
   ...glob.sync('examples/*/showcaseScenario.ts')
-    .map(item => baseConfig(item, 'dist/' + item.replace('.ts', '.js'))),
+    .map(item => baseConfig(item, 'dist/' + item.replace('.ts', '.js'), commonPlugins('tsconfig.examples.json'))),
 ];
