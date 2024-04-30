a12b180d436dd845a72e24c3dde2783eec0147bc
ondratra <ondratra@gmail.com>
2024-04-30 13:09:18 +0200

    build system update

--- rollup.config.ts
+++ rollup.config.ts
@@ -9,7 +9,7 @@ const baseConfig = (input: string, output: string, plugins: RollupPlugin[]) => (
   input: input,
   output: {
     file: output,
-    format: 'cjs',
+    format: 'es',
   },
   plugins,
 })
@@ -27,13 +27,18 @@ export default [
   {
     input: 'src/index.ts',
     output: {
-      file: 'dist/index.d.ts',
-      format: 'cjs',
+      //file: 'dist/index.d.ts',
+      dir: 'dist/',
+      format: 'es',
+
+      preserveModules: true
     },
     plugins: [dts()],
   },
 
   // examples
   ...glob.sync('examples/*/showcaseScenario.ts')
+    .filter(item => item.includes('001'))
+    //.slice(0, 1)
     .map(item => baseConfig(item, 'dist/' + item.replace('.ts', '.js'), commonPlugins('tsconfig.examples.json'))),
 ];
